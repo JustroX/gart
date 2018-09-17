22935cc3bf81ab64b32023d794860fc132a39792
Justine Che T. Romero <thepiesaresquared@gmail.com>
2018-09-17 23:37:43 +0800

    library added

---
+++ assets.js
@@ -0,0 +1,105 @@
+var particles  =
+{
+	"normal": 
+	{
+		x: 500 ,
+		y: 500 ,
+		r: 100,
+		style: "yellow",
+	},
+	"fractal":
+	{
+		x: 500,
+		y: 250,
+		r: 30,
+		id : "fractal",
+		stroke: "yellow",
+		style: "rgba(0,0,0,0)",
+		behaviors: ["fractal"],
+		lifetime: 10,
+		behavior_ops:
+		{
+			generation: 0,
+			direction : 0,
+			max_gen : 4,
+			distance: 100,
+			frequency: 4,
+		}
+	}
+};
+
+
+var behaviors = 
+{
+	random_walk : (o)=>
+	{
+		// console.log(o);
+		ops.mag = ops.mag || 1;
+		o.x += ops.mag*Math.random() - ops.mag/2;
+		o.y += ops.mag*Math.random() - ops.mag/2;	
+	},
+	fractal : (o) =>
+	{
+		if(o.time!=0)
+			return;
+		let g = o.parent;
+		let ops = o.behavior_ops;
+		let gen = ops.generation;
+		let direction = ops.direction;
+		let max_gen = ops.max_gen;
+		let d = ops.distance;
+		let f = ops.frequency;
+		let def = ((o_,i)=>{
+			let angle = (ops.newDirection && ops.newDirection(o_,i,direction,gen)) || (((gen == 0)? 2: 1) * ( i - (f-1)/2  )*Math.PI / f + direction);
+			let a = {
+				x : ( ops.newX && ops.newX(o_,i,o,gen.x,angle) ) || (o.x + d*Math.cos(angle)),
+				y : ( ops.newY && ops.newY(o_,i,o,gen.y,angle) ) || (o.y + d*Math.sin(angle)),
+				r :  ( ops.newR && ops.newR(o_,i,o,gen.r,angle) ) ||(o.r /2),
+				onstep: o.onstep,
+				draw : o.draw,
+				behavior_ops: 
+				{
+					distance : ( ops.newD && ops.newD(o_,i,d,gen, angle) ) || (d/2),
+					generation : gen + 1, 
+					direction: angle,
+					max_gen: max_gen,
+					frequency: ( ops.newF && ops.newF(o_,i,f,gen,angle) ) || f,
+					genesis: ops.genesis,
+
+					newX: ops.newX,
+					newY: ops.newY,
+					newR: ops.newR,
+					newDirection: ops.newDirection,
+					newD: ops.newD,
+					newF: ops.newF,
+				}
+			}
+			return a;
+		});
+		let on_genesis = ops.genesis;
+
+		if(gen < max_gen && o.time == 0)
+		setTimeout(()=>
+		{
+			for(let i =0 ; i<f; i++)
+			{
+				let new_vals  = def(o,i);
+				let old_vals  = {};
+				for( let i of ["x","y","r","vx","vy","style","stroke","ax","ay","tags","lifetime"] )
+				{
+					if(typeof o[i] != "function" && o[i] )
+						old_vals[i] = o[i];
+				}
+
+				 for( let i in new_vals)
+				 	old_vals[i] = new_vals[i];
+
+				let a = g.addParticle(g.getParticle( o.id , old_vals));
+				if(on_genesis) on_genesis(a,gen+1,i);
+			}
+		},1);
+	}
+}
+
+
+var GArt_default = { particles: particles, behaviors: behaviors };
--- gart.js
+++ gart.js
@@ -10,16 +10,30 @@
 
 class Gart
 {
-	constructor(options, _f)
+	constructor(options)
 	{
 		this.canvas = options.canvas;
 		this.ctx = this.canvas.getContext('2d');
 		this.fps  = options.fps | 30;
 		this.particles = [];
-		this.generator = [];
 		this.time = 0;
 		this.playing = false;
-		_f(this);
+		this.animate = options.animate || false;
+
+		this.canvas.addEventListener('click',(ev)=>{
+			if(!this.playing) this.start();
+			else this.pause();
+		});
+
+		this.resources  = new GModule();
+		this.modules = options.modules || [];
+		for(let i of this.modules)
+			this.resources.import(window[i]);
+
+		this.defined_step = options.onstep || ((g)=>{});
+
+		if(options.oncreate)
+			options.oncreate(this);
 	}
 
 	start()
@@ -34,6 +48,8 @@ class Gart
 
 	loop()
 	{
+		if(this.animate)
+			this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 		this.step();
 		this.draw();
 		this.time++;
@@ -42,8 +58,7 @@ class Gart
 
 	step()
 	{
-		for(let i of this.generator)
-			i.step(this.time);
+		this.defined_step(this);
 		for(let i of this.particles)
 			i.step(this.time);
 	}
@@ -51,19 +66,41 @@ class Gart
 	draw()
 	{
 		for(let i of this.particles)
-			i.draw(this.ctx);
+			i.draw(this.ctx,i);
 	}
 
 	//public
 
-	addParticle(p)
+	addParticle(p,ops)
 	{
+		if(typeof p == "string")
+			p = this.getParticle(p,ops);
 		p.parent = this;
 		let a = new Particle(p);
 		this.particles.push(a);
 		return a;
 	}
+
+	getStyleRGB(r,g,b,a)
+	{
+		return "rgba("+r+","+g+","+b+","+(a?a:1)+")";
+	}
+	getStyleHSL(h,s,l,a)
+	{
+		return "hsla("+Math.floor(h)%360+","+s+"%,"+l+"%,"+(a?a:1)+")";
+	}
+
+
+	getParticle(n,ops)
+	{
+		return this.resources.getParticle(n,ops);
+	}
+	getBehavior(n,ops)
+	{
+		return this.resources.getBehavior(n,ops);
+	}
 }
+
 
 class Particle
 {
@@ -72,21 +109,36 @@ class Particle
 		this.x = p.x || 0 ;
 		this.y = p.y || 0 ;
 		this.r = p.r || 10;
-		this.style = p.style || "rgba(0,0,0)";
+		this.style = p.style || "#ffffff";
+		this.stroke = p.stroke || "green";
 		this.vx = p.vx || 0 ;
 		this.vy = p.vy || 0 ;
 		this.ax  = p.ax || 0;
 		this.ay  = p.ay || 0;
-		this.behaviors = p.behaviors || [];
 		this.time = p.time | 0;
 		this.tags = p.tags || [];
 		this.parent = p.parent;
+		this.draw = p.draw || this.draw;
+		this.onstep = p.onstep || ((g)=>{});
+		this.lifetime = p.lifetime || -1;
+
+		for(let i in p)
+		{
+			if(!this[i])
+				this[i] = p[i];
+		}
+		this.behaviors = p.behaviors || [];
 	}
 
 	step(t)
 	{
 		for(let i of this.behaviors)
-			i(this);
+		{
+			if(typeof i == "string")
+				this.parent.getBehavior(i)(this,this.parent);
+			else
+			i(this,this.parent);
+		}
 
 		this.vx += this.ax;
 		this.vy += this.ay;
@@ -94,23 +146,47 @@ class Particle
 		this.y += this.vy;
 		this.r = Math.max(0,this.r);
 		this.time++;
+
+		this.onstep(this);
+
+		if( this.lifetime >0 && this.time >= this.lifetime)
+			this.destroy();
 	}
 
 	pull(mag,tag)
 	{
 		if(!this.parent) return;
+		let sx = 0, sy = 0;
 		for(let i of this.parent.particles)
-			if(i.includes(tag) || !tag )
+		{
+			// console.log(mag);
+			
+			if(i.tags.includes(tag) || !tag )
 			{
 				let dx = i.x - this.x;
 				let dy = i.y - this.y;
-				ax += dx;
-				ay += dy;
+				// console.log(dx);
+				let d = Math.sqrt(dx*dx + dy*dy);
+
+				if(d!=0)
+				{
+					sx += mag*dx/d;
+					sy += mag*dy/d;
+				}
 			}
+		}
+		this.ax = sx;
+		this.ay = sy;
 	}
 
+	destroy()
+	{
+		let index = this.parent.particles.indexOf(this);
+		// console.log(index);
+		this.parent.particles.splice(index,1);
+	}
 
-	draw(ctx)
+	draw(ctx,o)
 	{
 		ctx.beginPath();
 
@@ -123,14 +199,60 @@ class Particle
 		ctx.fillStyle = this.style;
 		ctx.fill();
 
+		ctx.strokeStyle = this.stroke;
+		ctx.stroke();
+
 		ctx.closePath();
 	}
 }
 
-class Generators
+/*
+{
+	"name":{ definition }, 
+}
+*/
+
+class GModule
 {
 	constructor()
 	{
-		
+		this.particles  = {};
+		this.behaviors = {};
 	}
+
+	import(p)
+	{
+		for( let i in p.particles )
+			this.particles[i] = p.particles[i];
+		for( let i in p.behaviors )
+			this.behaviors[i] = p.behaviors[i];
+	}
+
+	getParticle(n,ops)
+	{
+		if(!this.particles[n])
+			return console.log("Gart: Particle "+n+" not found.");
+		let a = JSON.parse(JSON.stringify( this.particles[n] ));
+		a.behaviors = this.particles[n].behaviors; 
+		for(let i in ops)
+		{
+			if(typeof ops[i] == 'object')
+				for(let j in ops[i])
+					a[i][j] = ops[i][j];
+			else
+					a[i] = ops[i];
+		}
+		return a;
+
+	}
+
+	getBehavior(n)
+	{
+		if(!this.behaviors[n])
+			return console.log("Gart: Behavior "+n+" not found.");
+		return this.behaviors[n]; 
+	}
+
+
+
 }
